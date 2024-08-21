73011e694e10b74a893697c5a878dbae6eb0af94
pankaj-davi <menariapankaj17@gmail.com>
2024-08-22 03:15:13 +0530

    reomve un used packages

--- server.js
+++ server.js
@@ -109,4 +109,4 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(8080, () => console.log(`Server running on port ${8080}`));
+server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
